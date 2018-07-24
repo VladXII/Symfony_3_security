9398fd676a72291e2ebeeb967acfccf01fd3aab8
VladXII <PobereznichenkoVladimir@gmail.com>
2018-07-24 19:34:01 +0300

    17 chapter finished

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vlad
+ * Date: 24.07.18
+ * Time: 18:28
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\User;
+use AppBundle\Form\UserRegistrationForm;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserController extends Controller
+{
+    /**
+     * @Route("/register", name="user_register")
+     */
+    public function registerAction(Request $request)
+    {
+        $form = $this->createForm(UserRegistrationForm::class);
+
+        $form->handleRequest($request);
+        if($form->isValid()){
+            /** @var User $user */
+            $user = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('succes', 'Welcome '.$user->getEmail());
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('user/register.html.twig',[
+            'form'=> $form->createView()
+        ]);
+    }
+}
